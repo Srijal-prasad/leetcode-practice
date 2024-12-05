081b6a22e6886ef34e975d2b9a986b256daa1ea1
Srijal Prasad <srijalprasad@gmail.com>
2024-12-05 21:48:35 +0530

    Time: 14 ms (82.94%), Space: 45.9 MB (18.75%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        int i = 0;
+        int j = 0;
+        int n = start.length();
+        while(i<n || j<n){
+            while(i<n && start.charAt(i)=='_') i++;
+            while(j<n && target.charAt(j)=='_') j++;
+
+            if(i==n || j==n){
+                return (i==j);
+            }
+
+            if((start.charAt(i)!=target.charAt(j)) || (start.charAt(i)=='L' && i<j) || (start.charAt(i)=='R' && i>j)){
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
